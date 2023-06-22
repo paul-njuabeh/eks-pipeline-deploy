49e60f9d3f15dd8a60af1e3cff22308e187a9723
paul-njuabeh <117046940+paul-njuabeh@users.noreply.github.com>
2023-06-21 21:48:36 -0700

    Update eks-cluster.tf

--- Terraform/eks-cluster.tf
+++ Terraform/eks-cluster.tf
@@ -7,7 +7,7 @@ module "eks" {
     cluster_endpoint_public_access  = true
 
     vpc_id = aws_vpc.myapp-vpc.id
-    subnet_ids = aws_subnet.myapp[1]
+    subnet_ids = aws_subnet.myapp[count.index(1)]
 
     tags = {
         environment = "development"
