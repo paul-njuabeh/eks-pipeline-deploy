47c4a940c48d865e057d4f8e85588f3d7a7f0495
paul-njuabeh <117046940+paul-njuabeh@users.noreply.github.com>
2023-06-22 04:11:45 -0700

    Update vpc.tf

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -22,7 +22,7 @@ resource "aws_vpc" "myapp-vpc" {
 
 resource "aws_subnet" "myapp" {
   cidr_block        = "10.0.1.0/24"
-  vpc_id               = aws_vpc.myapp-vpc.id # add vpc_id argument
+  vpc_id               = aws_vpc.myapp-vpc.var.subnet_ids # add vpc_id argument
   availability_zone    = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
   tags = {
